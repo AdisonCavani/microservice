10e1855ed0d742b8c697b05117a2796f05b77f26
Adison Cavani <adriansrodon@tuta.io>
2022-07-31 22:06:53 +0200

    Add IPlatformRepository with implementation

---
+++ backend/PlatformService/Repositories/IPlatformRepository.cs
@@ -0,0 +1,11 @@
+﻿using PlatformService.Database.Entities;
+
+namespace PlatformService.Repositories;
+
+public interface IPlatformRepository
+{
+    Task CreatePlatformAsync(Platform platform, CancellationToken ct = default);
+    Task<IEnumerable<Platform>> GetAllPlatformsAsync(CancellationToken ct = default);
+    Task<Platform?> GetPlatformByIdAsync(int id, CancellationToken ct = default);
+    Task<bool> SaveChangesAsync(CancellationToken ct = default);
+}
---
+++ backend/PlatformService/Repositories/PlatformRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Database;
+using PlatformService.Database.Entities;
+
+namespace PlatformService.Repositories;
+
+public class PlatformRepository : IPlatformRepository
+{
+    private readonly AppDbContext _context;
+
+    public PlatformRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreatePlatformAsync(Platform platform, CancellationToken ct = default)
+    {
+        ArgumentNullException.ThrowIfNull(platform);
+
+        // TODO: SaveChangesAsync() ???
+        await _context.Platforms.AddAsync(platform, ct);
+    }
+
+    public async Task<IEnumerable<Platform>> GetAllPlatformsAsync(CancellationToken ct = default)
+    {
+        return await _context.Platforms.ToListAsync(ct);
+    }
+
+    public async Task<Platform?> GetPlatformByIdAsync(int id, CancellationToken ct = default)
+    {
+        return await _context.Platforms.FirstOrDefaultAsync(x => x.Id == id, ct);
+    }
+
+    public async Task<bool> SaveChangesAsync(CancellationToken ct = default)
+    {
+        return await _context.SaveChangesAsync(ct) >= 0;
+    }
+}
