bf1879e155a7ad42e4f95d94d0a9fa485cd834be
Adison Cavani <adriansrodon@tuta.io>
2022-08-01 22:40:31 +0200

    Add static class with ApiRoutes

---
+++ backend/PlatformService/Contracts/ApiRoutes.cs
@@ -0,0 +1,17 @@
+﻿namespace PlatformService.Contracts;
+
+public static class ApiRoutes
+{
+    private const string Base = "/api";
+
+    public const string Health = $"{Base}/health";
+    
+    public static class Platform
+    {
+        private const string Endpoint = $"{Base}/platform";
+
+        public const string Create = $"{Endpoint}/create";
+        public const string Get = $"{Endpoint}/get";
+        public const string GetAll = $"{Endpoint}/getAll";
+    }
+}
